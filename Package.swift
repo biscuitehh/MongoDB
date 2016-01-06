1d1f99fabdbfcabb6f83f0f5aa4ca83480dcb46d
Michael Thomas <michael.thomas@willowtreeapps.com>
2016-01-05 20:30:28 -0500

    One more try

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "MongoDB",
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/CMongoDBC.git", majorVersion: 1),
+        .Package(url: "https://github.com/PureSwift/CMongoC.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/SwiftFoundation.git", majorVersion: 1)
     ],
     targets: [
